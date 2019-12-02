7333fbd20076a01896d2d024a402679bc6e345f2
Aurelien Thierry <aurelien.thierry@quoscient.io>
2019-12-02 14:09:05 +0100

    Improved options

--- src/tools/prototype_file_ts.c
+++ src/tools/prototype_file_ts.c
@@ -4,24 +4,32 @@
 #include "prototype_file_ts.h"
 
 int main (int argc, char **argv){
-    
-  if (argc >= 4){
-#ifdef __linux__
-    printf("%s: (statx AT_SYMLINK_NOFOLLOW)\n", argv[1]);
-    print_path_timestamps_statx_ns(argv[1], 0);
-#endif
-  }
-  else if (argc >= 3){
-    printf("%s: (lstat + B)\n", argv[1]);
-    print_path_timestamps_lstat_ns(argv[1]);
-  }
-  else if (argc >= 2){
-    printf("%s: (stat + B)\n", argv[1]);
-    print_path_timestamps_ns(argv[1]);
-  }
-  else {
-    printf("ERROR: need one argument (path)\n");
-  }  
+   
+    if (argc < 2){
+        printf("ERROR: need one argument (path)\n");
+        return 1;
+    }
+  
+    char* path = argv[1];
+    int mode = 0;
+    if (argc >= 3){
+        mode = atoi(argv[2]);
+    }
+
+    if (mode == 1){
+        printf("%s: (lstat + B)\n", path);
+        print_path_timestamps_lstat_ns(path);
+    }
+    else if (mode == 2){
+        #ifdef __linux__
+        printf("%s: (statx AT_SYMLINK_NOFOLLOW)\n", argv[1]);
+        print_path_timestamps_statx_ns(argv[1], 0);
+        #endif
+    }
+    else {
+        printf("%s: (stat + B)\n", path);
+        print_path_timestamps_ns(path);
+    }
 }
 
 
