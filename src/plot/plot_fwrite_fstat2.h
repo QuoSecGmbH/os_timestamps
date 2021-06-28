cee6f4f09c6e1e00f173e9810b296f4b1271c635
Aurelien Thierry <code@athierry.fr>
2021-06-28 12:43:29 +0000

    Added plot_fwrite_fstat2.c

---
+++ src/plot/plot_fwrite_fstat2.h
@@ -0,0 +1,17 @@
+#ifndef PLOT_FWRITE_FSTAT2_H
+#define PLOT_FWRITE_FSTAT2_H
+
+#include "current_time.h"
+#include "file_ts.h"
+#include "log.h"
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <sys/stat.h>
+#include "misc.h"
+#include <getopt.h>
+
+struct timespec* delta(struct timespec* ts_wait);
+int main (int argc, char **argv);
+
+#endif
